7d4b8129820942c6731e31b08cd6a56d2d701464
Raymond-The-Best <user.squire@gmail.com>
2024-03-06 12:18:00 +0100

    Finished Ensuring old browser compatibility

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -21,6 +21,16 @@ const path = require("path");
     },
     module: {
         rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env"],
+              },
+            },
+          },
             {
                 test: /\.(html)$/,
                 use: ["html-loader"],
